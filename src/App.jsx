9846df385358a3a581f9f7a29f835cd824e188c0
Jonnathan Hernández González <llagi1234@gmail.com>
2023-11-23 15:41:43 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jonnathan Hernandez Gonzalez",
+  title: "Studying Programming",
+  email: "llagi1234@gmail.com",
+  gitHub: "llagi1234",
+  instagram: "llagi1234",
+  linkedIn: "llagi1234",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "llagi1234",
 };
 
 const primaryColor = "#4E567E";
